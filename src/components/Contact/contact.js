8b87f884290e848409da0b3d267c55583ad81853
Carlos <demirovic84@gmail.com>
2019-07-24 10:44:49 +0200

    Update contact.js

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './contact.css';
+import image from './coding.jpg';
+
 
 
 
@@ -10,16 +12,19 @@ import './contact.css';
 class ContactComponent extends Component {
   render() {
     return (
+
         
         <div className="component-wrapper contact-component" id="contact">
             <div className="title">
                 <h1>Contact</h1>
+                <img src={image} alt="Logo" />
             </div>
-        
-        
+
         </div>
     );
+
   }
+
 }
 
 export default ContactComponent;
